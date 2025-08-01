4f35ba7437610ef0d7f4749e2b207c902cbbf90f
contigen <69436315+contigen@users.noreply.github.com.>
2025-08-01 15:33:47 +0100

    Add loading component with animated text

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div className='grid place-content-center h-[calc(100dvh-4rem)]'>
+      <span className='text-2xl font-bold bg-gradient-to-r from-amber-600 via-rose-500 to-stone-600 bg-clip-text text-transparent animate-pulse duration-1000'>
+        Aurea
+      </span>
+    </div>
+  )
+}
